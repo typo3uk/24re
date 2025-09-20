e98ef210a681d5c7d3815f644d0cbed900a5ebde
TYPO3 Integrator <typo3@ukr.net>
2025-09-20 15:44:28 +0300

    Rename by

--- app/components/footer/Credit.tsx
+++ app/components/footer/Credit.tsx
@@ -7,11 +7,11 @@ const Credit = () => {
       <div className="text-center text-sm py-5">
         A project by{" "}
         <Link
-          href="https://www.sammyleths.com"
+          href="https://24re.vercel.app/"
           target="_blank"
           className="cursor-pointer font-bold hover:text-blue-600"
         >
-          {"<SammyLeths />"}
+          {"24re"}
         </Link>
       </div>
     </Container>
